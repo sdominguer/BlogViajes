1c8d95aafc53451d1b167e159a3469471eac3dbc
saradrl <sdominguer@eafit.edu.co>
2025-05-01 11:19:12 -0500

    enviar correos de contacto

--- app.py
+++ app.py
@@ -23,7 +23,7 @@ app.config['MAIL_SERVER'] = 'smtp.gmail.com'
 app.config['MAIL_PORT'] = 587
 app.config['MAIL_USE_TLS'] = True
 app.config['MAIL_USERNAME'] = 'anasofiacasrpo@gmail.com'         # PON AQUÍ TU CORREO
-app.config['MAIL_PASSWORD'] = 'tu_contraseña_de_aplicacion' # CONTRASEÑA DE APP, no tu password normal
+app.config['MAIL_PASSWORD'] = 'zbqu xmyg orsq wlrd' # CONTRASEÑA DE APP, no tu password normal
 app.config['MAIL_DEFAULT_SENDER'] = 'anasofiacasrpo@gmail.com'
 
 mail = Mail(app)
